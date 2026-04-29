a84d6e19c3256f8ffcf22013febc42ccb5cbea8b
Julio Franz <juliofranzdev@gmail.com>
2026-04-29 12:22:11 -0300

    feat(settings): update email configuration to use getenv for improved flexibility

--- LIMS/settings.py
+++ LIMS/settings.py
@@ -155,14 +155,13 @@ LOGGING = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['EMAIL_HOST']
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'localhost')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False') == 'True'
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
-EMAIL_TIMEOUT = 30
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL','')
+BREVO_API_KEY = os.getenv('BREVO_API_KEY')
 
 LANGUAGE_CODE = 'en-us'
 
